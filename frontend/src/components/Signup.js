32d8fb4eda592e16f65026ee67eb1cbade3801aa
arshitshukla <shuklaarshit@gmail.com>
2023-09-02 09:45:02 +0530

    Changes to host

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -12,7 +12,7 @@ const Signup = (props) => {
     const handlesubmit=async (e)=>{
         e.preventDefault();
         const {name,email,password}=credentials;
-        const response=await fetch("http://localhost:5000/api/auth/createUser",{
+        const response = await fetch("http://localhost:5000/api/auth/createUser",{
             
             method:"POST",
             headers:{
