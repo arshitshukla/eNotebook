625c853b7a03e9107ff9c25094654ddcf71f0ada
Arshit Shukla <109364576+arshitshukla@users.noreply.github.com>
2023-09-02 10:02:37 +0530

    Update Login.js hostName

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -11,7 +11,7 @@ const Login = (props) => {
 
     const handlesubmit=async (e)=>{
         e.preventDefault();
-        const response=await fetch("http://localhost:5000/api/auth/login",{
+        const response=await fetch("https://enotebook-xtz4.onrender.com/api/auth/login",{
             method:"POST",
             headers:{
                 "Content-Type":"application/json"
